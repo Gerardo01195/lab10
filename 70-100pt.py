9bbe613b953e6db707cf30f3ddf6d8ffea7dc69c
Gerardo01195 <gerardo416@yahoo.com>
2014-10-21 14:21:28 -0500

    Gerardo_Anzorena_Lab10

--- 70-100pt.py
+++ 70-100pt.py
@@ -22,7 +22,21 @@ root = Tk()
 drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=1)
 
-# Insert your code here to draw the house!
-
-
+# house and roof
+square = drawpad.create_rectangle(200,200,400,400, fill ='red')
+line1 = drawpad.create_line(200,200,300,100)
+line2 = drawpad.create_line(300,100,400,200)
+# square windows code
+square1 = drawpad.create_rectangle(220,300,260,340, fill ='white')
+square2 = drawpad.create_rectangle(340,300,380,340, fill ='white')
+square3 = drawpad.create_rectangle(220,250,260,210, fill ='white')
+square4 = drawpad.create_rectangle(340,250,380,210, fill ='white')
+# Door Code
+square = drawpad.create_rectangle(280,400,320,320, fill ='red')
+# Door Handle
+oval = drawpad.create_oval(310,350,320,360, fill='white')
+# Chimney code!
+line1 = drawpad.create_line(400,110,400,200)
+line2 = drawpad.create_line(350,110,400,110)
+line3 = drawpad.create_line(350,110,350,150)
 root.mainloop()
